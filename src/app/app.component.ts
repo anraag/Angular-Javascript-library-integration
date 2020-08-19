b13ee460012c9feefdc6c01246ab4a7cee3fe7f5
Anurag Yadav <anurag.yadav@bison.howard.edu>
2020-08-19 13:01:00 -0500

    added Javascript library to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+declare var ChessBoard: any;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'js-lib-example';
+export class AppComponent implements OnInit {
+  title = "js-lib-example";
+  board: any;
+
+  ngOnInit(): void {
+    this.board = ChessBoard("board1", {
+      position: "start",
+      draggable: true,
+    });
+  }
 }
